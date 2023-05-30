aabc1740087ea39100c7a04eef1f396529c22308
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-05-30 13:55:31 +0300

    getting info from URL
    getting info from URL

--- ETL_Demo.py
+++ ETL_Demo.py
@@ -16,3 +16,18 @@ try:
 except Exception as e:
     print('could not read ETLDemo.ini' + str(e))
     sys.exit()
+
+# reading settings from config file
+start_date = config['CONFIG']['start_date']
+url = config['CONFIG']['url']
+server = config['CONFIG']['server']
+database = config['CONFIG']['database']
+
+# request data from url
+try:
+    Response = requests.get(url+start_date)
+except Exception as e:
+    print('could not read request')
+    sys.exit()
+
+print(Response.text)
